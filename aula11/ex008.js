52806ce4977006483a29bf34bba27f52a10781f2
Joao-Pedro-OC <joaopedrocavalcante630@gmail.com>
2025-01-19 06:01:31 -0300

    ok metade da aula 2
    terminei nos condiçoes múltiplas

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 100
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { // Condição Simples
+    console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
